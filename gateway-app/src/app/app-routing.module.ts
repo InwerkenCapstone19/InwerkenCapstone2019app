b7ff257e078798768d1c0f2334353d0160237174
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-03-21 19:21:57 -0500

    Added routing for the 3 components

--- gateway-app/src/app/app-routing.module.ts
+++ gateway-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {DiscoverComponent} from './discover/discover.component';
+import {AnalyticsComponent} from './analytics/analytics.component'
+const routes: Routes = [
+	{path: '', component:HomeComponent },
+	{path: '/discover', component:DiscoverComponent},
+	{path: '/analytics', component:AnalyticsComponent}
+ ];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
