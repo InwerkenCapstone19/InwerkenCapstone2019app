5b6fb51037afd8f7cd15927e9ee420e9aa62124f
NikoliHeiser <nikoli.heiser@ndus.edu>
2019-03-26 17:30:39 -0500

    added the rest of the button links and added to the home, Assignment, service and settings

--- gateway-app/src/app/app-routing.module.ts
+++ gateway-app/src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import {HomeComponent} from './home/home.component';
 import {DiscoverComponent} from './discover/discover.component';
 import {AnalyticsComponent} from './analytics/analytics.component'
+import {AssignmentComponent} from './assignment/assignment.component'
+import {ServiceComponent} from './service/service.component'
+import {SettingsComponent} from './settings/settings.component'
+
+
 const routes: Routes = [
 	{path: 'home', component:HomeComponent},
 	{path: '',redirectTo: '/home', pathMatch: 'full' },
+	{path: 'analytics', component:AnalyticsComponent},
 	{path: 'discover', component:DiscoverComponent},
-	{path: 'analytics', component:AnalyticsComponent}
+	{path: 'assignment', component:AssignmentComponent},
+        {path: 'service', component:ServiceComponent},
+        {path: 'settings', component:SettingsComponent}
  ];
 
 
--- gateway-app/src/app/app.module.ts
+++ gateway-app/src/app/app.module.ts
@@ -3,20 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AnalyticsComponent } from './analytics/analytics.component';
+
 import { HomeComponent } from './home/home.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
 import { DiscoverComponent } from './discover/discover.component';
+import { AssignmentComponent } from './assignment/assignment.component'
 import { ServiceComponent } from './service/service.component';
+import { SettingsComponent } from './settings/settings.component';
+
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SensorTypeSelectorComponent } from './sensor-type-selector/sensor-type-selector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnalyticsComponent,
     HomeComponent,
+    AnalyticsComponent,
     DiscoverComponent,
+    AssignmentComponent,
     ServiceComponent,
+    SettingsComponent,
     NavBarComponent,
     SensorTypeSelectorComponent
   ],
