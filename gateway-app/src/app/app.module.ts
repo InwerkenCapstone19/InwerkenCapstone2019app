ac6b5cd087251270269117fb2c97eb63f3e26dfa
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-03-31 13:59:47 -0500

    Added default component

--- gateway-app/src/app/app.module.ts
+++ gateway-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SettingsComponent } from './settings/settings.component';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SensorTypeSelectorComponent } from './sensor-type-selector/sensor-type-selector.component';
+import { SensorTableComponent } from './sensor-table/sensor-table.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { SensorTypeSelectorComponent } from './sensor-type-selector/sensor-type-
     ServiceComponent,
     SettingsComponent,
     NavBarComponent,
-    SensorTypeSelectorComponent
+    SensorTypeSelectorComponent,
+    SensorTableComponent
   ],
   imports: [
     BrowserModule,
