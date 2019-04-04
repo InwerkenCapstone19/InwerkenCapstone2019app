fe9152648e718ec7d064d504667fb74334fa6038
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-04-04 12:34:10 -0500

    changed table to use services

--- gateway-app/src/app/sensor-table/sensor-table.component.ts
+++ gateway-app/src/app/sensor-table/sensor-table.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { DataReadingService } from '../data-reading.service';
+import { SensorService } from '../sensor.service';
+import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, 
+         MatSortModule, MatTableModule } from '@angular/material';
+import { forkJoin } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-sensor-table',
@@ -7,9 +14,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SensorTableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataServ: DataReadingService, private sensorServ: SensorService) { }
+
+  tableInfo = {sensors:null,readings:null};
 
   ngOnInit() {
+  	this.getTableInfo();
   }
 
+  getTableInfo(){
+  	this.dataServ.getDataReadings("Kanban").subscribe(readings => this.tableInfo.readings = readings);
+  	this.sensorServ.getSensors().subscribe(sensors => this.tableInfo.sensors = sensors);
+    console.log(this.tableInfo);
+  }
+
+  //name and order of columns in table
+  displayedColumns: string[] = ['sensID', 'name', 'reading'];
 }
