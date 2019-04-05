94680eb3328142189999d0ca74b356fef24e5ea3
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-04-05 14:23:25 -0500

    modified sensor service to get by sensorId and/or unitName

--- gateway-app/src/app/sensor.service.ts
+++ gateway-app/src/app/sensor.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Sensor } from './sensor.model'
 
 
 @Injectable({
@@ -9,14 +10,27 @@ export class SensorService {
 
   constructor(private _http:HttpClient) { }
 
-  public getSensors(sensorId:number=null){
- 	let _url = 'https://172.103.0.2/red/units'
+  public getSensors(sensorId:number=null,unitName:string=null){
+ 	let _url = 'https://172.103.0.2/red/sensors'
 
-  	//if no id is passed, get all sensors, otherwise only get sensor with ID
-  	if(sensorId!==null){
-  		return this._http.get(_url+'id='+sensorId);
-  	}else{
-  		return this._http.get(_url);
-  	}
+   //if at least one parameter is passed
+   if(sensorId !== null || unitName !== null ){
+     //if both parameters are passed
+     if (sensorId !== null && unitName !== null){
+       return this._http.get<Sensor[]>(_url+`?id={{sensorId}}&unitName={{unitName}}`);
+     }
+     //if only sensorId is passed
+     else if (sensorId!==null){
+       return this._http.get<Sensor[]>(_url+`?id={{sensorId}}`);
+     }
+     //if only unitName is passed
+     else if (unitName!==null){
+       return this._http.get<Sensor[]>(_url+`?unitName={{unitName}}`);
+     }
+   }
+   //no parameters passed and get all sensors
+   else{
+     return this._http.get<Sensor[]>(_url);
+   }
   }
 }
