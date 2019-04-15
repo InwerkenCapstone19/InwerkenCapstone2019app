b0a144af2375872b87876c01656c26167a456993
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-04-15 11:39:14 -0500

    Initial commit

---
+++ gateway-app/src/app/data-graph/data-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-graph',
+  templateUrl: './data-graph.component.html',
+  styleUrls: ['./data-graph.component.css']
+})
+export class DataGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
