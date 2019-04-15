ec8edb3532d67506308935fab495c77a9cc93258
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-04-15 12:17:07 -0500

    implemented method to get and store data in correct format

--- gateway-app/src/app/data-graph/data-graph.component.ts
+++ gateway-app/src/app/data-graph/data-graph.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
+import { DataReadingService } from '../data-reading.service';
 
 @Component({
   selector: 'app-data-graph',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-graph.component.css']
 })
 export class DataGraphComponent implements OnInit {
-
-  constructor() { }
+  
+  chartData = [{time:null,reading:null}]
+  
+  constructor(private data:DataReadingService ) { }
 
   ngOnInit() {
+  	this.getChartData();
+  	console.log(this.chartData);
+  }
+
+  //calls dataReadingService instance and assigns that
+  getChartData(sensorType:string="Kanban",sensorId:number=null){
+  	this.data.getDataReadings(sensorType,sensorId).subscribe(res => 
+  		{	//for each value in the array returned by the data service
+  			for(let singleReading of res){
+  				//add it's date and value to chartData
+  				this.chartData.push({time:singleReading.dateTime, reading:singleReading.value});
+  			}
+  		}
+  	);
   }
 
 }
