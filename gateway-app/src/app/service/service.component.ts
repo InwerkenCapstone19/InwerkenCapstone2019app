d0e74ee866826d132700b2f02c8a2aa7cb80c191
Taylor Schmidt <taylor.n.schmidt@ndsu.edu>
2019-04-17 08:55:27 -0500

    Added map component to service page

--- gateway-app/src/app/service/service.component.ts
+++ gateway-app/src/app/service/service.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { trigger,state,style,animate,transition} from '@angular/animations';
 
+import { MapComponent } from '../map/map.component';
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
